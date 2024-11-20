3b100cbd27ad5726cab0512aecaf9059231ec717
Nick the Sick <nicholas.perez@tiptap.dev>
2024-11-20 22:46:12 +0100

    refactor: use react-query for data fetching

--- src/client/components/SearchBox.tsx
+++ src/client/components/SearchBox.tsx
@@ -1,9 +1,24 @@
-import { type FC, useState } from "hono/jsx/dom";
+import { useState, type FC } from "hono/jsx/dom";
 
 import type { BookResult } from "../../scrapers";
+import { useQuery } from "@tanstack/react-query";
 
 export const SearchBox: FC<{}> = () => {
-  const [searchItems, setSearchItems] = useState<BookResult[]>([]);
+  const [query, setQuery] = useState("");
+  const bookResults = useQuery({
+    staleTime: 10000,
+    queryKey: ["searchBooks", query],
+    queryFn: async ({ signal }) => {
+      if (query.length < 3) {
+        return [];
+      }
+      const res = await fetch(
+        `/xrpc/buzz.bookhive.searchBooks?q=${query}&limit=7`,
+        { signal },
+      );
+      return (await res.json()) as BookResult[];
+    },
+  });
 
   return (
     <div class="relative ml-3">
@@ -17,35 +32,40 @@ export const SearchBox: FC<{}> = () => {
           placeholder="Search books..."
           id="search-books"
           class="block w-64 rounded-md border-0 py-1.5 pl-8 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 dark:placeholder:text-gray-800"
-          onChange={async (e) => {
-            const query = (e.target as HTMLInputElement).value;
-            if (query.length < 3) {
-              return;
-            }
-
-            try {
-              const res = await fetch(
-                `/xrpc/buzz.bookhive.searchBooks?q=${query}&limit=7`,
-              );
-              const data: BookResult[] = await res.json();
-              setSearchItems(data);
-            } catch (error) {
-              console.error("Search failed:", error);
-              setSearchItems([]);
-            }
+          onChange={(e) => {
+            setQuery((e.target as HTMLInputElement).value);
           }}
         />
       </div>
-      {Boolean(searchItems.length) && (
+      {!!bookResults.data?.length && (
         <ul
           id="search-results"
           role="list"
           class="absolute left-0 z-10 mt-2 w-[calc(100%+64px)] origin-top-right divide-y divide-gray-100 rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none dark:divide-gray-700 dark:bg-slate-700"
         >
-          {searchItems.map((book) => (
+          {bookResults.data.map((book) => (
             <li class="px-1 py-2">
               <a
                 href={`/book/${book.id}`}
+                onClick={async (e) => {
+                  e.preventDefault();
+
+                  const response = await fetch(`/books`, {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      author: book.authors.join(", "),
+                      title: book.title,
+                      year: book.publishedDate,
+                      // isbn: book.identifiers.goodreads,
+                      coverImage: book.cover || book.thumbnail,
+                      // hiveId: book.id,
+                    }),
+                  });
+                  console.log(await response.json());
+                }}
                 class="flex items-center justify-between gap-x-6 space-x-4 rounded-md px-2 py-3 hover:bg-slate-800"
               >
                 <div class="flex items-center justify-between space-x-4">
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,9 +1,19 @@
 import { render } from "hono/jsx/dom";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SearchBox } from "./components/SearchBox";
+
+const queryClient = new QueryClient();
 
 document.addEventListener("DOMContentLoaded", () => {
   const mountSearchBox = document.getElementById("mount-search-box");
   if (mountSearchBox) {
-    render(<SearchBox />, mountSearchBox);
+    render(
+      // Provide the client to your App
+      <QueryClientProvider client={queryClient}>
+        <SearchBox />
+      </QueryClientProvider>,
+      mountSearchBox,
+    );
   }
 });
