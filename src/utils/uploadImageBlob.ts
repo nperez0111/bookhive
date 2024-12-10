e3fedd3d18234516ff86ef69716f9ea7f2b1b2a8
Nick the Sick <nicholas.perez@tiptap.dev>
2024-12-10 22:14:22 +0100

    fix: add try catch and logging to uploading a blob

--- src/utils/uploadImageBlob.ts
+++ src/utils/uploadImageBlob.ts
@@ -1,25 +1,37 @@
 import { Agent } from "@atproto/api";
-import type { BlobRef } from "@atproto/lexicon";
 import sharp from "sharp";
+import { getLogger } from "../logger";
+
+const logger = getLogger({ name: "upload-image-blob" });
 
 export async function uploadImageBlob(image: string | undefined, agent: Agent) {
-  let imageBlobRef: BlobRef | undefined = undefined;
-  if (image) {
-    const data = await fetch(image as string).then((res) => res.arrayBuffer());
+  try {
+    if (image) {
+      logger.trace("trying to fetch image", { image });
+      const data = await fetch(image as string).then((res) =>
+        res.arrayBuffer(),
+      );
+      logger.trace("downloaded image");
 
-    const resizedImage = await sharp(data)
-      .resize({ width: 800, withoutEnlargement: true })
-      .jpeg()
-      .toBuffer();
-    const uploadResponse = await agent.com.atproto.repo.uploadBlob(
-      resizedImage,
-      {
-        encoding: "image/jpeg",
-      },
-    );
-    if (uploadResponse.success) {
-      imageBlobRef = uploadResponse.data.blob;
+      const resizedImage = await sharp(data)
+        .resize({ width: 800, withoutEnlargement: true })
+        .jpeg()
+        .toBuffer();
+      logger.trace("resized image");
+
+      const uploadResponse = await agent.com.atproto.repo.uploadBlob(
+        resizedImage,
+        {
+          encoding: "image/jpeg",
+        },
+      );
+      logger.trace("reupload image", { success: uploadResponse.success });
+      if (uploadResponse.success) {
+        return uploadResponse.data.blob;
+      }
     }
+  } catch (err) {
+    logger.error("failed to upload image blob", { err });
   }
-  return imageBlobRef;
+  return undefined;
 }
