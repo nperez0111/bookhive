eb4e5b17e2ec8c265a1745fd79b327b73f335a1e
Nick the Sick <computers@nickthesick.com>
2025-04-21 12:41:24 +0200

    chore: bump tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,11 @@
 import tailwindAnimated from "tailwindcss-animated";
-import plugin from "tailwindcss/plugin";
 import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      textShadow: {
-        sm: "0 1px 2px var(--tw-shadow-color)",
-        md: "0 2px 4px var(--tw-shadow-color)",
-        lg: "0 8px 16px var(--tw-shadow-color)",
-      },
-    },
-  },
+  content: ["./src/index.css", "./src/pages/**/*.{js,ts,jsx,tsx}", "./src/client/**/*.{js,ts,jsx,tsx}"],
   plugins: [
     tailwindAnimated,
     forms,
-    plugin(function ({ matchUtilities, theme }) {
-      matchUtilities(
-        {
-          "text-shadow": (value) => ({
-            textShadow: value,
-          }),
-        },
-        { values: theme("textShadow") },
-      );
-    }),
   ],
 };
