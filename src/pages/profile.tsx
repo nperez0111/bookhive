06a7d5b59032ab3e570c3404a66a8cb324a8b2b0
Nick the Sick <computers@nickthesick.com>
2025-05-15 20:44:01 +0200

    fix: show to users who are not yet buzzers

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -45,54 +45,53 @@ export const ProfilePage: FC<{
         </div>
       </div>
 
-      {isBuzzer ? (
-        <div class="flex flex-col gap-10">
-          {isOwner && (
-            <div class="px-4 lg:px-8">
-              To export your Goodreads library,{" "}
-              <a
-                href="https://www.goodreads.com/review/import"
-                class="inline text-blue-800 hover:underline"
-                target="_blank"
-              >
-                export your library
-              </a>{" "}
-              and then you can import it here:
-              <label
-                class="ml-3 inline-block cursor-pointer rounded-md bg-yellow-50 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                role="menuitem"
-                tabindex={-1}
-                id="user-menu-item-2"
-              >
-                <span id="import-label">Import Goodreads CSV</span>
-                <span id="importing-label" class="hidden">
-                  Importing...
-                  <svg
-                    class="ml-2 inline-block h-4 w-4 animate-spin"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      class="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      stroke-width="4"
-                      fill="none"
-                    />
-                    <path
-                      class="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    />
-                  </svg>
-                </span>
-                <input
-                  type="file"
-                  name="export"
-                  accept=".csv"
-                  class="hidden"
-                  onchange={`
+      <div class="flex flex-col gap-10">
+        {isOwner && (
+          <div class="px-4 lg:px-8">
+            To export your Goodreads library,{" "}
+            <a
+              href="https://www.goodreads.com/review/import"
+              class="inline text-blue-800 hover:underline"
+              target="_blank"
+            >
+              export your library
+            </a>{" "}
+            and then you can import it here:
+            <label
+              class="ml-3 inline-block cursor-pointer rounded-md bg-yellow-50 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              role="menuitem"
+              tabindex={-1}
+              id="user-menu-item-2"
+            >
+              <span id="import-label">Import Goodreads CSV</span>
+              <span id="importing-label" class="hidden">
+                Importing...
+                <svg
+                  class="ml-2 inline-block h-4 w-4 animate-spin"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    class="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                    fill="none"
+                  />
+                  <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  />
+                </svg>
+              </span>
+              <input
+                type="file"
+                name="export"
+                accept=".csv"
+                class="hidden"
+                onchange={`
                 const form = new FormData();
                 form.append('export', this.files[0]);
                 document.getElementById('import-label').classList.add('hidden');
@@ -124,65 +123,68 @@ export const ProfilePage: FC<{
                   alert('Error importing books: ' + error.message);
                 });
               `}
-                />
-              </label>
-            </div>
-          )}
-          <section class="mt-8 flex flex-col gap-2 px-4 lg:px-8">
-            <div class="mb-6">
-              <h2 class="text-4xl font-bold lg:text-5xl lg:tracking-tight">
-                Library
-              </h2>
-            </div>
-            <BookList books={books} />
-          </section>
-          {books.some((book) => book.review) && (
-            <section class="mt-16 flex flex-col gap-2 px-4 lg:px-8">
+              />
+            </label>
+          </div>
+        )}
+        {isBuzzer ? (
+          <>
+            <section class="mt-8 flex flex-col gap-2 px-4 lg:px-8">
               <div class="mb-6">
                 <h2 class="text-4xl font-bold lg:text-5xl lg:tracking-tight">
-                  Reviews
+                  Library
                 </h2>
               </div>
-              {books
-                .filter((book) => book.review)
-                .map((book) => {
-                  return (
-                    <div class="group mb-2 cursor-pointer rounded-lg border border-slate-200 bg-yellow-50 p-4 shadow-sm transition hover:border-slate-300 hover:shadow-md dark:border-slate-700 dark:bg-zinc-800">
-                      <a href={`/books/${book.hiveId}`} class="flex gap-4">
-                        {book.cover || book.thumbnail ? (
-                          <img
-                            src={`${book.cover || book.thumbnail || ""}`}
-                            // src={`/images/w_300/${book.cover || book.thumbnail || ""}`}
-                            alt=""
-                            class="h-36 w-24 rounded-lg object-cover shadow-sm"
-                          />
-                        ) : (
-                          <FallbackCover className="h-36 w-24" />
-                        )}
-                        <span class="flex flex-col gap-1">
-                          <span class="text-lg font-medium group-hover:text-sky-600 dark:group-hover:text-sky-400">
-                            {book.title}
-                          </span>
-                          <span class="text-sm text-slate-600 dark:text-slate-400">
-                            by {book.authors.split("\t").join(", ")}
-                            {book.stars ? (
-                              <span class="text-md mx-1 text-slate-800 dark:text-slate-200">
-                                ({book.stars / 2} ⭐)
-                              </span>
-                            ) : null}
-                          </span>
-                          <p class="py-2">{book.review}</p>
-                        </span>
-                      </a>
-                    </div>
-                  );
-                })}
+              <BookList books={books} />
             </section>
-          )}
-        </div>
-      ) : (
-        <div class="text-center">😔 This user has no books on bookhive</div>
-      )}
+            {books.some((book) => book.review) && (
+              <section class="mt-16 flex flex-col gap-2 px-4 lg:px-8">
+                <div class="mb-6">
+                  <h2 class="text-4xl font-bold lg:text-5xl lg:tracking-tight">
+                    Reviews
+                  </h2>
+                </div>
+                {books
+                  .filter((book) => book.review)
+                  .map((book) => {
+                    return (
+                      <div class="group mb-2 cursor-pointer rounded-lg border border-slate-200 bg-yellow-50 p-4 shadow-sm transition hover:border-slate-300 hover:shadow-md dark:border-slate-700 dark:bg-zinc-800">
+                        <a href={`/books/${book.hiveId}`} class="flex gap-4">
+                          {book.cover || book.thumbnail ? (
+                            <img
+                              src={`${book.cover || book.thumbnail || ""}`}
+                              // src={`/images/w_300/${book.cover || book.thumbnail || ""}`}
+                              alt=""
+                              class="h-36 w-24 rounded-lg object-cover shadow-sm"
+                            />
+                          ) : (
+                            <FallbackCover className="h-36 w-24" />
+                          )}
+                          <span class="flex flex-col gap-1">
+                            <span class="text-lg font-medium group-hover:text-sky-600 dark:group-hover:text-sky-400">
+                              {book.title}
+                            </span>
+                            <span class="text-sm text-slate-600 dark:text-slate-400">
+                              by {book.authors.split("\t").join(", ")}
+                              {book.stars ? (
+                                <span class="text-md mx-1 text-slate-800 dark:text-slate-200">
+                                  ({book.stars / 2} ⭐)
+                                </span>
+                              ) : null}
+                            </span>
+                            <p class="py-2">{book.review}</p>
+                          </span>
+                        </a>
+                      </div>
+                    );
+                  })}
+              </section>
+            )}
+          </>
+        ) : (
+          <div class="text-center">😔 This user has no books on bookhive</div>
+        )}
+      </div>
     </div>
   );
 };
