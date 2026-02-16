956b70da03f8eb8351f6e67c3e843d9a03e56413
Nick the Sick <computers@nickthesick.com>
2026-02-16 13:46:43 +0100

    fix: nitro v3

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ import { env } from "./env";
 
 import entryHtml from "./entry.html";
 
-const server = Bun.serve({
+export default {
   port: env.PORT,
   development: env.isDevelopment,
   routes: {
     "/_bundle": entryHtml,
   },
   fetch: instrument(app).fetch,
-});
+};
 
-console.log(`Server is running on ${server.url}`);
+// console.log(`Server is running on ${server.url}`);
